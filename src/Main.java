298e407c56c73280d09c90821b0cae48e7211f19
Idea <632218194@qq.com>
2023-11-16 11:02:08 +0800

    http test v4

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello, World3!";
+            String response = "Hello, World4!";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
